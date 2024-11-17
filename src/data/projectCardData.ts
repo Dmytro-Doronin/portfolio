d9f359a684efc8c51e3b1b217f6a76141eb98538
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-11-17 16:10:46 +0100

    blog card added

--- src/data/projectCardData.ts
+++ src/data/projectCardData.ts
@@ -1,6 +1,6 @@
 import todo2 from '../images/todo2.png'
 import cards from '../images/cards.png'
-
+import blogs from '../images/blogs.png'
 type ProjectCardDataType = {
     img: string
     title: string
@@ -24,6 +24,15 @@ export const projectCardData: Array<ProjectCardDataType> = [
         linkOnGithub: 'https://github.com/Dmytro-Doronin/flash-cards',
         linkOnDemo: 'https://flash-cards-ruby-two.vercel.app/',
         text: 'This is a project - Learning by Cards. Technologies: React, Redux, RTK-Query, SCSS, TS.'
+    },
+    {
+        img: blogs,
+        title: 'Blog Platform',
+        linkOnGithub: 'https://github.com/Dmytro-Doronin/blog-frontend-Angular',
+        linkOnDemo: 'https://blog-frontend-angular-eight.vercel.app/main/blogs-page',
+        text: 'A modern blog platform where users can create, edit, and manage blogs and posts. The platform supports user authentication, ' +
+            'file uploads (images), and dynamic content loading.' +
+            'Built with Angular for the frontend, Nest.js for the backend, and MongoDB as the database. The project also integrates with AWS S3 for media storage.'
     },
 
 
